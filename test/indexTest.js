6d1627c3f605410c95a372b915aeade4246d19c5
Bismark <bettkipkoech45@gmail.com>
2025-03-20 12:21:10 +0300

    hoisting

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,7 +8,7 @@ describe("index.js", () => {
 
     it("uses the default activity 'roller-skate' when no arguments are passed", function() {
       expect(saturdayFun()).to.equal("This Saturday, I want to roller-skate!")
-    })
+    }
 
     it("permits the default activity to be overriden", function() {
       expect(saturdayFun("bathe my dog")).to.equal("This Saturday, I want to bathe my dog!")
